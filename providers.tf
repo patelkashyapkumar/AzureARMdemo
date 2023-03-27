c065506176d12042ae080b24215b7e0af87930e5
patelkashyapkumar <64489085+patelkashyapkumar@users.noreply.github.com>
2023-03-27 15:02:23 +0100

    cahnges to provider

--- providers.tf
+++ providers.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3"
+    }
+  }
+
+  cloud {
+    organization = "hcl-ws-demo"
+
+    workspaces {
+      name = "AzureARMdemo"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
